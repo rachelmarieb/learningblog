0501ee2a39154c56fddd818561f57ec66f5a0a7c
rachel <rachelbalik@gmail.com>
2015-10-07 19:10:54 -0700

    updated design

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
 get '/about' => 'welcome#about'
 get '/contact' => 'welcome#contact'
+get '/posts' => 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
